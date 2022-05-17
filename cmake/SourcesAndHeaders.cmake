e7fb8f9a1a8cf01bc7055bbdd727af1c5cefcb21
Neccolini <shun11202991@gmail.com>
2022-05-17 16:58:42 +0900

    feat: added file info

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,14 +1,24 @@
 set(sources
-    src/tmp.cpp
+    src/Network.cpp
+    src/MultiProcess.cpp
+    src/devices/hardware/Serial.cpp
+    src/devices/Devices.cpp
+    src/devices/Router.cpp
+    src/devices/Cordinator.cpp
+    src/main.cpp
 )
 
 set(exe_sources
-		src/main.cpp
 		${sources}
 )
 
 set(headers
-    include/project/tmp.hpp
+    include/shapechangeable-simulation/Network.h
+    include/shapechangeable-simulation/MultiProcess.h
+    include/shapechangeable-simulation/devices/hardware/Serial.h
+    include/shapechangeable-simulation/devices/Devices.h
+    include/shapechangeable-simulation/devices/Cordinator.h
+    include/shapechangeable-simulation/devices/Router.h
 )
 
 set(test_sources
