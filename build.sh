25ea8d8468420428675f1587a07b4520bff9ed55
Neccolini <shun11202991@gmail.com>
2022-05-17 18:17:55 +0900

    feat: shell script for building

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir build && cd build
+sudo apt-get update
+sudo apt-get install cmake libgtest-dev
+pushd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo cp lib *.a /usr/lib/
+popd
+sudo cmake ..
+sudo cmake --build . --target install
+exit 0
