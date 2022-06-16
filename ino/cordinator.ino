aa512524f286d4777f21bb718fcb046881d118e1
Neccolini <shun11202991@gmail.com>
2022-06-16 15:28:20 +0900

    feat: algorithm ver.1.0

--- ino/cordinator.ino
+++ ino/cordinator.ino
@@ -1,18 +1,74 @@
-bool can_send = false;
-void setup() { Serial.begin(9600); }
-
-void loop() {
-  if (!can_send) {
-    String req = "RTS";
-    Serial2.print(req);
+class packet {
+ public:
+  int prv_id;
+  int src_id;
+  int dst_id;
+  bool resend;
+  int pnt_id;
+  int trg_pnt_id;
+  String msg;
+  packet(String msg, int prv_id, int src_id, int dst_id, bool resend, int pnt_id, int trg_pnt_id = -1) {
+    this->msg = msg;
+    this->prv_id = prv_id;
+    this->src_id = src_id;
+    this->dst_id = dst_id;
+    this->resend = resend;
+    this->pnt_id = pnt_id;
+    this->trg_pnt_id = trg_pnt_id;
   }
+  packet(String raw_msg) {
+    std::vector<String> list = raw_msg.split(',');
+    this->prv_id = list[0].toInt();
+    this->src_id = list[1].toInt();
+    this->dst_id = list[2].toInt();
+    this->pnt_id = list[3].toInt();
+    this->resend = list[4] == String("true") ? true : false;
+    this->msg = list[5];
+  }
+  String into_string() {
+    String ret = "/";
+    ret += ItoString(this->prv_id);
+    ret += ",";
+    ret += ItoString(this->src_id);
+    ret += ",";
+    ret += ItoString(this->dst_id);
+    ret += ",";
+    ret += ItoString(this->pnt_id);
+    ret += ",";
+    ret += String(this->resend ? "true" : "false");
+    ret += ",";
+    ret += this->msg;
+    ret += "/";
+    return ret;
+  }
+};
+
+int my_id = 0;
+int id_count_up = 1;
+bool resend = false;
+map<int, int> id_tnt;  // random id -> 真のid
+
+void setup() {}
+void loop() {
+  std::printf("my_id -> %d pid -> %d\n", my_id, getpid());
   if (Serial2.available() > 0) {
-    String rep = Serial2.readString();
-    if (rep.indexOf("CTS") != -1) {
-      can_send = true;
-      String data = "this is a data.";
-      Serial2.print(data);
+    vector<String> req_list = Serial2.readString().split('/');
+    for (auto req_str : req_list) {  // 来たパケットを順に処理していく
+      if (req_str == "") continue;
+      packet req = packet(req_str);
+      if (req.dst_id != my_id) continue;  // todo ルーティング
+      if (req.msg == "joinrequest") {
+        if (id_tnt[req.src_id] == 0) {
+          id_tnt[req.src_id] = id_count_up++;
+        }
+        String msg = "Grant_id ";
+        msg += ItoString(id_tnt[req.src_id]);
+        int dst_pnt_id = req.pnt_id == -1 ? 0 : req.pnt_id;
+        packet rep = packet(msg, req.prv_id, my_id, req.src_id, resend, -1, dst_pnt_id);
+        Serial2.print(rep.into_string());
+      }
     }
   }
-  sleep(3);
+  sleep(4);
+  // std::exit(0);
 }
--- ino/router.ino
+++ ino/router.ino
@@ -1,17 +1,82 @@
-bool waiting_data = false;
-void setup() { Serial.begin(9600); }
+class packet {
+ public:
+  int prv_id;
+  int src_id;
+  int dst_id;
+  bool resend;
+  int pnt_id;
+  int trg_pnt_id;
+  String msg;
+  packet(String msg, int prv_id, int src_id, int dst_id, bool resend, int pnt_id, int trg_pnt_id = -1) {
+    this->msg = msg;
+    this->prv_id = prv_id;
+    this->src_id = src_id;
+    this->dst_id = dst_id;
+    this->resend = resend;
+    this->pnt_id = pnt_id;
+    this->trg_pnt_id = trg_pnt_id;
+  }
+  packet(String raw_msg) {
+    std::vector<String> list = raw_msg.split(',');
+    this->prv_id = list[0].toInt();
+    this->src_id = list[1].toInt();
+    this->dst_id = list[2].toInt();
+    this->pnt_id = list[3].toInt();
+    this->resend = list[4] == String("true") ? true : false;
+    this->msg = list[5];
+  }
+  String into_string() {
+    String ret = "/";
+    ret += ItoString(this->prv_id);
+    ret += ",";
+    ret += ItoString(this->src_id);
+    ret += ",";
+    ret += ItoString(this->dst_id);
+    ret += ",";
+    ret += ItoString(this->pnt_id);
+    ret += ",";
+    ret += String(this->resend ? "true" : "false");
+    ret += ",";
+    ret += this->msg;
+    ret += "/";
+    return ret;
+  }
+};
+
+const int CORDINATOR_ID = 0;
+bool joined = false;
+bool resend = false;
+int my_id;
+int pnt_id;
+map<int, int> dst_mp;  // 目的のid -> その方向の隣接ノードid
+
+void setup() {
+  random_device rnd;
+  int random_value = rnd();
+  random_value = random_value < 0 ? -random_value : random_value;
+  my_id = random_value < 10000 ? random_value + 10000 : random_value;
+}
 
 void loop() {
+  std::printf("my_id -> %d pid -> %d\n", my_id, getpid());
+  if (!joined) {
+    // コーディネータに対し参加要求を送る
+    packet join_request = packet(String("joinrequest"), my_id, my_id, CORDINATOR_ID, resend, -1);
+    // Serial.println(join_request.into_string());
+    Serial2.print(join_request.into_string());
+  }
   if (Serial2.available() > 0) {
-    String recv;
-    recv = Serial2.readString();
-    if (!waiting_data) {
-      if (recv.indexOf("RTS") != -1) {
-        Serial2.print("CTS");
-        waiting_data = true;
+    vector<String> pkt_list = Serial2.readString().split('/');
+    for (auto pkt : pkt_list) {
+      if (pkt == "") continue;
+      packet p = packet(pkt);
+      if (p.dst_id != my_id) continue;  // todo ルーティング
+      vector<String> msg_list = p.msg.split(' ');
+      if (msg_list[0] == String("Grant_id")) {  // 参加要求の返事 真IDの取得
+        my_id = msg_list[1].toInt();
+        joined = true;
+      } else if (msg_list[0] == String("")) {
       }
-    } else {
-      waiting_data = false;
     }
   }
   sleep(3);
