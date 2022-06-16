7d39b45c1ad1200f798d4b222d0f03515b4ba74b
Neccolini <shun11202991@gmail.com>
2022-06-16 15:24:38 +0900

    fix: organized device sources

--- src/devices/Cordinator.cpp
+++ src/devices/Cordinator.cpp
@@ -6,34 +6,18 @@
 #include "shapechangeable-simulation/devices/hardware/Serial.h"
 
 namespace Cordinator {
+using namespace std;
+using namespace string_extensions;
 HardwareSerial Serial;
 // _GLOBAL_VARIABLES_BEGIN_ THIS COMMENT SHOULD NOT BE DELETED.
-bool can_send = false;
-
 // _GLOBAL_VARIABLES_END_ THIS COMMENT SHOULD NOT BE DELETED.
 inline void setup(HardwareSerial2& Serial2) {
   // _SETUP_BEGIN_ THIS COMMNET SHOULD NOT BE DELETED.
- Serial.begin(9600); 
   // _SETUP_END_ THIS COMMENT SHOULD NOT BE DELETED.
 }
 inline void loop(HardwareSerial2& Serial2) {
   while (true) {
     // _LOOP_BEGIN_ THIS COMMNET SHOULD NOT BE DELETED.
-
-  if (!can_send) {
-    String req = "RTS";
-    Serial2.print(req);
-  }
-  if (Serial2.available() > 0) {
-    String rep = Serial2.readString();
-    if (rep.indexOf("CTS") != -1) {
-      can_send = true;
-      String data = "this is a data.";
-      Serial2.print(data);
-    }
-  }
-  sleep(3);
-
     // _LOOP_END_ THIS COMMNET SHOULD NOT BE DELETED.
   }
 }
--- src/devices/Router.cpp
+++ src/devices/Router.cpp
@@ -6,34 +6,18 @@
 #include "shapechangeable-simulation/devices/hardware/Serial.h"
 
 namespace Router {
+using namespace std;
+using namespace string_extensions;
 HardwareSerial Serial;
 // _GLOBAL_VARIABLES_BEGIN_ THIS COMMENT SHOULD NOT BE DELETED.
-bool waiting_data = false;
-
 // _GLOBAL_VARIABLES_END_ THIS COMMENT SHOULD NOT BE DELETED.
 inline void setup(HardwareSerial2& Serial2) {
   // _SETUP_BEGIN_ THIS COMMNET SHOULD NOT BE DELETED.
- Serial.begin(9600); 
   // _SETUP_END_ THIS COMMENT SHOULD NOT BE DELETED.
 }
 inline void loop(HardwareSerial2& Serial2) {
   while (true) {
     // _LOOP_BEGIN_ THIS COMMNET SHOULD NOT BE DELETED.
-
-  if (Serial2.available() > 0) {
-    String recv;
-    recv = Serial2.readString();
-    if (!waiting_data) {
-      if (recv.indexOf("RTS") != -1) {
-        Serial2.print("CTS");
-        waiting_data = true;
-      }
-    } else {
-      waiting_data = false;
-    }
-  }
-  sleep(3);
-
     // _LOOP_END_ THIS COMMNET SHOULD NOT BE DELETED.
   }
 }
