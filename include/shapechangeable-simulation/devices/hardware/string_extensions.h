37c42c0b95ce2fb70d9e7606f9aa9594cff9de80
Neccolini <shun11202991@gmail.com>
2022-06-16 15:27:44 +0900

    feat: add string methods

--- include/shapechangeable-simulation/devices/hardware/string_extensions.h
+++ include/shapechangeable-simulation/devices/hardware/string_extensions.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace string_extensions {
 using namespace std;
@@ -11,7 +12,7 @@ struct string : std::string {
 
  public:
   void print() { std::cout << *this << std::endl; }
-  int indexOf(std::string s) {
+  int indexOf(string s) {
     for (int i = 0; i < this->size() - s.size() + 1, i < this->size(); i++) {
       if (this->substr(i, s.size()) == s) {
         return i;
@@ -19,5 +20,43 @@ struct string : std::string {
     }
     return -1;
   }
+  vector<string> split(char sep = ' ') {
+    string s = *this;
+    vector<string> elems;
+    string item;
+    for (auto ch : s) {
+      if (ch == sep) {
+        if (!item.empty()) elems.push_back(item);
+        item.clear();
+      } else {
+        item += ch;
+      }
+    }
+    if (!item.empty()) elems.push_back(item);
+    return elems;
+  }
+  int toInt() {
+    int ret = 0;
+    string s = *this;
+    int n = s.length();
+    int digit = 1;
+    if (s[0] == '-') {
+      for (int i = 0; i < n - 1; i++) {
+        char d = s[n - i - 1];
+        ret += (int)(d - '0') * digit;
+        digit *= 10;
+      }
+      ret = -ret;
+    } else {
+      for (int i = 0; i < n; i++) {
+        char d = s[n - i - 1];
+        ret += (d - '0') * digit;
+        digit *= 10;
+      }
+    }
+    return ret;
+  }
 };
 }  // namespace string_extensions
+
+string_extensions::string ItoString(int x);
--- src/devices/hardware/string_extensions.cpp
+++ src/devices/hardware/string_extensions.cpp
@@ -1,1 +1,26 @@
 #include "shapechangeable-simulation/devices/hardware/string_extensions.h"
+
+string_extensions::string ItoString(int x) {
+  string_extensions::string s;
+  if (x == 0)
+    s = string_extensions::string("0");
+  else if (x < 0) {
+    x = -x;
+    while (x) {
+      int a = x % 10;
+      x /= 10;
+      char digit[] = {'0' + a, '\0'};
+      s += string_extensions::string(digit);
+    }
+    s += "-";
+  } else {
+    while (x) {
+      int a = x % 10;
+      x /= 10;
+      char digit[] = {'0' + a, '\0'};
+      s += string_extensions::string(digit);
+    }
+  }
+  reverse(s.begin(), s.end());
+  return s;
+}
