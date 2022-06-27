29a62cf98da14386dc7df4d75a29cd178aece6d6
Neccolini <shun11202991@gmail.com>
2022-06-27 18:52:35 +0900

    feat: sample network

--- ino/cordinator.ino
+++ ino/cordinator.ino
@@ -1,20 +1,22 @@
 class packet {
  public:
   int prv_id;
+  int prv_prv_id;
   int src_id;
   int dst_id;
   bool resend;
   int pnt_id;
-  int trg_pnt_id;
+  int nbr_dst_id;
   String msg;
-  packet(String msg, int prv_id, int src_id, int dst_id, bool resend, int pnt_id, int trg_pnt_id = -1) {
+  packet(String msg, int prv_id, int src_id, int dst_id, bool resend, int pnt_id, int prv_prv_id = -1, int nbr_dst_id = -1) {
     this->msg = msg;
     this->prv_id = prv_id;
     this->src_id = src_id;
     this->dst_id = dst_id;
     this->resend = resend;
     this->pnt_id = pnt_id;
-    this->trg_pnt_id = trg_pnt_id;
+    this->prv_prv_id = prv_prv_id;
+    this->nbr_dst_id = nbr_dst_id;
   }
   packet(String raw_msg) {
     std::vector<String> list = raw_msg.split(',');
@@ -24,6 +26,8 @@ class packet {
     this->pnt_id = list[3].toInt();
     this->resend = list[4] == String("true") ? true : false;
     this->msg = list[5];
+    this->prv_prv_id = list[6].toInt();
+    this->nbr_dst_id = list[7].toInt();
   }
   String into_string() {
     String ret = "/";
@@ -38,6 +42,10 @@ class packet {
     ret += String(this->resend ? "true" : "false");
     ret += ",";
     ret += this->msg;
+    ret += ",";
+    ret += ItoString(this->prv_prv_id);
+    ret += ",";
+    ret += ItoString(this->nbr_dst_id);
     ret += "/";
     return ret;
   }
@@ -46,29 +54,47 @@ class packet {
 int my_id = 0;
 int id_count_up = 1;
 bool resend = false;
-map<int, int> id_tnt;  // random id -> 真のid
+map<int, int> random_id_table;  // random id -> 真のid
+map<int, int> dst_id_table;     // 目的のid -> その方向の隣接ノードid
+inline bool update_table(int key_id, int val_id, map<int, int> table) { table[key_id] = val_id; }
+inline bool table_find(int id, map<int, int> table) {
+  if (table.find(id) == table.end()) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 void setup() {}
 void loop() {
-  std::printf("my_id -> %d pid -> %d\n", my_id, getpid());
+  // std::printf("my_id -> %d pid -> %d\n", my_id, getpid());
   if (Serial2.available() > 0) {
     vector<String> req_list = Serial2.readString().split('/');
     for (auto req_str : req_list) {  // 来たパケットを順に処理していく
       if (req_str == "") continue;
-      packet req = packet(req_str);
-      if (req.dst_id != my_id) continue;  // todo ルーティング
-      if (req.msg == "joinrequest") {
-        if (id_tnt[req.src_id] == 0) {
-          id_tnt[req.src_id] = id_count_up++;
+      packet p = packet(req_str);
+      if (p.prv_prv_id == my_id) continue;  // 自分が送信したパケットは受け取らない
+      if (p.dst_id != my_id && (p.nbr_dst_id == my_id || p.nbr_dst_id == -1)) {  // ルーティング
+        dst_id_table[p.src_id] = p.prv_id;
+        int nbr_dst_id = table_find(p.dst_id, dst_id_table) ? dst_id_table[p.dst_id] : -1;
+        Serial2.print(packet(p.msg, my_id, p.src_id, p.dst_id, p.resend, p.pnt_id, p.prv_id, nbr_dst_id).into_string());
+        // std::printf("myid: %d, routing: %s\n",my_id, packet(p.msg, my_id, p.src_id, p.dst_id, p.resend, p.pnt_id, p.prv_id, nbr_dst_id).into_string().c_str());
+        continue;
+      }
+      if (p.msg == "joinrequest") {  // 参加要求に対し，真のidを返す
+        if (!table_find(p.src_id, random_id_table)) {
+          random_id_table[p.src_id] = id_count_up++;
+          // std::printf("assigned %d to %d\n", random_id_table[p.src_id], p.src_id);
         }
+        // std::printf("assigned %d to %d\n", random_id_table[p.src_id], p.src_id);
         String msg = "Grant_id ";
-        msg += ItoString(id_tnt[req.src_id]);
-        int dst_pnt_id = req.pnt_id == -1 ? 0 : req.pnt_id;
-        packet rep = packet(msg, req.prv_id, my_id, req.src_id, resend, -1, dst_pnt_id);
+        msg += ItoString(random_id_table[p.src_id]);
+        int dst_pnt_id = p.pnt_id == -1 ? 0 : p.pnt_id;
+        packet rep = packet(msg, my_id, my_id, p.src_id, resend, -1, dst_pnt_id);
         Serial2.print(rep.into_string());
       }
     }
   }
-  sleep(4);
+  sleep(2);
   // std::exit(0);
 }
--- ino/router.ino
+++ ino/router.ino
@@ -1,20 +1,22 @@
 class packet {
  public:
   int prv_id;
+  int prv_prv_id;
   int src_id;
   int dst_id;
   bool resend;
   int pnt_id;
-  int trg_pnt_id;
+  int nbr_dst_id;
   String msg;
-  packet(String msg, int prv_id, int src_id, int dst_id, bool resend, int pnt_id, int trg_pnt_id = -1) {
+  packet(String msg, int prv_id, int src_id, int dst_id, bool resend, int pnt_id, int prv_prv_id = -1, int nbr_dst_id = -1) {
     this->msg = msg;
     this->prv_id = prv_id;
     this->src_id = src_id;
     this->dst_id = dst_id;
     this->resend = resend;
     this->pnt_id = pnt_id;
-    this->trg_pnt_id = trg_pnt_id;
+    this->prv_prv_id = prv_prv_id;
+    this->nbr_dst_id = nbr_dst_id;
   }
   packet(String raw_msg) {
     std::vector<String> list = raw_msg.split(',');
@@ -24,6 +26,8 @@ class packet {
     this->pnt_id = list[3].toInt();
     this->resend = list[4] == String("true") ? true : false;
     this->msg = list[5];
+    this->prv_prv_id = list[6].toInt();
+    this->nbr_dst_id = list[7].toInt();
   }
   String into_string() {
     String ret = "/";
@@ -38,6 +42,10 @@ class packet {
     ret += String(this->resend ? "true" : "false");
     ret += ",";
     ret += this->msg;
+    ret += ",";
+    ret += ItoString(this->prv_prv_id);
+    ret += ",";
+    ret += ItoString(this->nbr_dst_id);
     ret += "/";
     return ret;
   }
@@ -48,8 +56,15 @@ bool joined = false;
 bool resend = false;
 int my_id;
 int pnt_id;
-map<int, int> dst_mp;  // 目的のid -> その方向の隣接ノードid
-
+map<int, int> dst_id_table;  // 目的のid -> その方向の隣接ノードid
+bool update_table(int dst_id, int nbr_id) { dst_id_table[dst_id] = nbr_id; }
+bool table_find(int dst_id) {
+  if (dst_id_table.find(dst_id) == dst_id_table.end()) {
+    return false;
+  } else {
+    return true;
+  }
+}
 void setup() {
   random_device rnd;
   int random_value = rnd();
@@ -70,14 +85,38 @@ void loop() {
     for (auto pkt : pkt_list) {
       if (pkt == "") continue;
       packet p = packet(pkt);
-      if (p.dst_id != my_id) continue;  // todo ルーティング
-      vector<String> msg_list = p.msg.split(' ');
-      if (msg_list[0] == String("Grant_id")) {  // 参加要求の返事 真IDの取得
-        my_id = msg_list[1].toInt();
-        joined = true;
-      } else if (msg_list[0] == String("")) {
+      if (p.prv_prv_id == my_id) continue;  // 自分が送ったパケットは受け取らない
+
+      if (p.dst_id != my_id && (p.nbr_dst_id == my_id || p.nbr_dst_id == -1)) {  // ルーティング
+        update_table(p.src_id, p.prv_id);                                        // todo どのタイミングでどうやってテーブル更新する?
+        int nbr_dst_id = table_find(p.dst_id) ? dst_id_table[p.dst_id] : -1;
+        Serial2.print(packet(p.msg, my_id, p.src_id, p.dst_id, p.resend, p.pnt_id, p.prv_id, nbr_dst_id).into_string());
+        // std::printf("myid: %d, routing: %s\n",my_id, packet(p.msg, my_id, p.src_id, p.dst_id, p.resend, p.pnt_id, p.prv_id, nbr_dst_id).into_string().c_str());
+        continue;
+      }
+
+      if (p.dst_id == my_id) {  // 自分宛てだった場合
+        vector<String> msg_list = p.msg.split(' ');
+        if (msg_list[0] == String("Grant_id") && !joined) {  // 真IDの取得
+          update_table(p.src_id, p.prv_id);                  // todo どのタイミングでどうやってテーブル更新する?
+          my_id = msg_list[1].toInt();
+          std::printf("myid: %d, message %s received from %d\n", my_id, p.msg.c_str(), p.src_id);
+          pnt_id = p.prv_id;
+          joined = true;
+        } else {
+          std::printf("myid: %d message %s received from %d\n", my_id, p.msg.c_str(), p.src_id);
+        }
       }
     }
   }
-  sleep(3);
+  if (joined) {
+    int dst_id = 1;
+    if (dst_id != my_id) {
+      int nbr_dst_id = table_find(dst_id) ? dst_id_table[dst_id] : -1;
+      packet test_p = packet("test", my_id, my_id, 1, false, pnt_id, -1, nbr_dst_id);
+      std::printf("myid: %d, sending msg to %d\n", my_id, dst_id);
+      Serial2.print(test_p.into_string());
+    }
+  }
+  sleep(1);
 }
